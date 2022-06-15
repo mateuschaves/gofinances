1265428aa56df0bccfabe5a392677f517b56bee5
Mateus Henrique <mateushenriquechaves1@gmail.com>
2022-06-15 00:25:34 -0300

    feat: migrating to typescript and  add global styled components config

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components/native';
+
+import Dashboard from './src/screens/Dashboard';
+
+import theme from './src/global/styles/theme';
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  );
+}
+
