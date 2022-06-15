92cd2c9330602aa81f2877dfbdf20621ad1ce9c2
Mateus Henrique <mateushenriquechaves1@gmail.com>
2022-06-15 11:53:45 -0300

    feat: add transaction list

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import HighlightCard from '../components/HighlightCard';
+import TransactionCard from '../components/TransactionCard';
 
-import { Avatar, Container, Header, Icon, User, UserGreeting, UserInfo, UserName, UserWrapper, HighlightCards } from './styles';
+import { Avatar, Container, Header, Icon, User, UserGreeting, UserInfo, UserName, UserWrapper, HighlightCards, Transactions, Title, TransactionList } from './styles';
+import { TransactionCardProps } from '../components/TransactionCard/index';
+
+export interface DataListProps extends TransactionCardProps {
+  id: number;
+}
 
 export default function Dashboard() {
+
+  const transactions: DataListProps[] = [{
+    id: 1,
+    type: 'positive',
+    title: 'Desenvolvimento de site',
+    amount: 'R$ 12.000,00',
+    category: { name: 'Vendas', icon: 'dollar-sign' }, 
+    date: '13/04/2020'
+  },
+  {
+    id: 2,
+    type: 'negative',
+    title: 'Hamgueria Pizzy',
+    amount: 'R$ 59,00',
+    category: { name: 'Alimentação', icon: 'coffee' }, 
+    date: '13/04/2020'
+  },
+  {
+    id: 3,
+    type: 'negative',
+    title: 'Aluguel do apartamento',
+    amount: 'R$ 1.200,00',
+    category: { name: 'Casa', icon: 'shopping-bag' }, 
+    date: '13/04/2020'
+  }]
+
   return (
     <Container>
       <Header>
@@ -24,6 +56,16 @@ export default function Dashboard() {
         <HighlightCard title='Saídas' amount='R$ 1.259,00' lastTransaction='Última entrada dia 3 de abril' type='down'/>
         <HighlightCard title='Total' amount='R$ 16.141,00' lastTransaction='Última entrada dia 13 de abril' type='total' />
       </HighlightCards>
+
+      <Transactions>
+          <Title>Transações</Title>
+
+          <TransactionList
+            data={transactions}
+            renderItem={({ item }) => <TransactionCard data={item} />}
+            keyExtractor={item => item.id.toString()}
+          />
+      </Transactions>
     </Container>
   )
 }
---
+++ src/screens/components/TransactionCard/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+import { 
+    Container,
+    Title,
+    Amount,
+    Footer,
+    Category,
+    CategoryName,
+    Icon,
+    Date,
+} from './styles'
+
+interface Category {
+    name: string;
+    icon: string;
+}
+
+export interface TransactionCardProps {
+    type: 'positive' | 'negative';
+    title: string;
+    amount: string;
+    category: Category;
+    date: string
+}
+
+interface Props {
+    data: TransactionCardProps
+}
+
+export default function TransactionCard({ data }: Props) {
+  const { title, amount, category, date, type } = data
+
+  return (
+    <Container>
+        <Title>{title}</Title>
+
+        <Amount type={type}>
+            {type === 'negative' && '- '}
+            {amount}
+        </Amount>
+
+        <Footer>
+            <Category>
+                <Icon name={category.icon}/>
+                <CategoryName>{category.name}</CategoryName>
+            </Category>
+
+            <Date>{date}</Date>
+        </Footer>
+    </Container>
+  )
+}
